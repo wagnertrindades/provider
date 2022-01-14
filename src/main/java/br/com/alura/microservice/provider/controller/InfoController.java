e88bce35efc1f4104ded055a0270405c24b34d4e
wtrindades <wtrindades@gmail.com>
2022-01-14 20:40:27 -0300

    chore: add logs in OrderController and InfoController

--- src/main/java/br/com/alura/microservice/provider/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/provider/controller/InfoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.provider.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,16 @@ import br.com.alura.microservice.provider.service.InfoService;
 @RestController
 @RequestMapping("/info")
 public class InfoController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
 
 	@Autowired
 	private InfoService infoService;
 	
 	@RequestMapping("/{state}")
 	public InfoProvider getInfoByState(@PathVariable String state) {
+		
+		LOG.info("received info order of provider localized in {}", state);
 		return infoService.getInfoByState(state);
 	}
 }
--- src/main/java/br/com/alura/microservice/provider/controller/OrderController.java
+++ src/main/java/br/com/alura/microservice/provider/controller/OrderController.java
@@ -2,6 +2,8 @@ package br.com.alura.microservice.provider.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +19,16 @@ import br.com.alura.microservice.provider.service.OrderService;
 @RequestMapping("order")
 public class OrderController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(OrderController.class);
+
+	
 	@Autowired
 	private OrderService orderService;
 	
 	@PostMapping
 	public Order createOrder(@RequestBody List<OrderItemDTO> products) {
+		
+		LOG.info("order received");
 		return orderService.createOrder(products);
 	}
 	
